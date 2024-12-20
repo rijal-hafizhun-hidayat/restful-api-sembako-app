8cefef38481819ff7ea56e13f79d7f355e38a77b
unknown <rijal.1344@gmail.com>
2024-12-20 20:41:14 +0700

    setup: mvc structure

--- src/app/web.ts
+++ src/app/web.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { errorMiddleware } from "../middleware/error-middleware";
+import { publicRoute } from "../router/public-router";
 
 const web = express();
 
 web.use(express.json());
+web.use(publicRoute);
+web.use(errorMiddleware);
 
 export { web };

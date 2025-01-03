80c02e4923bd713e74d6e4281e768d78e83b3841
unknown <rijal.1344@gmail.com>
2025-01-03 13:21:57 +0700

    feat: add destroy and get all transaction

--- src/controller/transaction-controller.ts
+++ src/controller/transaction-controller.ts
@@ -20,4 +20,43 @@ export class TransactionController {
       next(error);
     }
   }
+
+  static async getAllTransaction(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const result = await TransactionService.getAllTransaction();
+      return res.status(200).json({
+        statusCode: 200,
+        message: "success get transaction",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async destroyTransactionByTransactionId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const transactionId: number = parseInt(
+        req.params.transactionId as string
+      );
+      const result = await TransactionService.destroyTransactionByTransactionId(
+        transactionId
+      );
+      return res.status(200).json({
+        statusCode: 200,
+        message: "success destroy transaction",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
